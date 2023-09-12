d4610cf5b0fb8324f9755458dc6d2de5b3bb0ce1
Daria Rekhlova <dolly1993@yandex.ru>
2023-09-12 20:04:10 +0400

    Добавили Homework4

---
+++ Homework4/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        GeometryFigure figureBox = new GeometryFigure();
+        Circle circle1 = new Circle(10);
+        figureBox.add(circle1);
+
+        figureBox.getInfo(0);
+
+
+
+    }
+}
